c2291723316eae3e4d4f26558c026ddeb8ec85b5
Scheals <piecoo@gmail.com>
2022-04-16 21:28:10 +0200

    Refactor no change tests and add King tests

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -4,8 +4,53 @@ require_relative '../lib/rook'
 require_relative '../lib/bishop'
 require_relative '../lib/piece'
 require_relative '../lib/queen'
+require_relative '../lib/king'
 
-# rubocop: disable Layout/LineLength, Metrics/BlockLength
+# rubocop: disable Layout/LineLength, Metrics/BlockLength, Lint/AmbiguousBlockAssociation
+describe King do
+  describe '#move' do
+    context 'when moved vertically by one tile' do
+      subject(:vertical_move) { described_class.new('a1') }
+      it 'changes its position' do
+        vertical_move.move('a2')
+        expect(vertical_move.position).to be('a2')
+      end
+      it 'does not move further than one tile' do
+        expect{ vertical_move.move('a6') }.to_not change { vertical_move.position }
+      end
+    end
+    context 'when moved horizontally by one tile' do
+      subject(:horizontal_move) { described_class.new('b1') }
+      it 'changes its position' do
+        horizontal_move.move('c1')
+        expect(horizontal_move.position).to be('c1')
+      end
+      it 'does not move further than one tile' do
+        expect{ horizontal_move.move('h1') }.to_not change { horizontal_move.position }
+      end
+    end
+    context 'when moved diagonally by one tile' do
+      subject(:diagonal_move) { described_class.new('c4') }
+      it 'changes its position' do
+        diagonal_move.move('d5')
+        expect(diagonal_move.position).to be('d5')
+      end
+      it 'does not move further than one tile' do
+        expect{ diagonal_move.move('e6') }.to_not change { diagonal_move.position }
+      end
+    end
+    context 'when told to move on its own position' do
+      subject(:immovable_move) { described_class.new('K0') }
+      it "doesn't change its position" do
+        expect { immovable_move.move('K0') }.to_not change { immovable_move.position }
+      end
+      it 'returns nil' do
+        expect(immovable_move.move('K0')).to be(nil)
+      end
+    end
+  end
+end
+
 describe Queen do
   describe '#move' do
     context 'when moved vertically' do
@@ -24,7 +69,7 @@ describe Queen do
     end
     context 'when moved diagonally' do
       subject(:diagonal_move) { described_class.new('c4') }
-      it "changes its position" do
+      it 'changes its position' do
         diagonal_move.move('d5')
         expect(diagonal_move.position).to be('d5')
       end
@@ -32,8 +77,7 @@ describe Queen do
     context 'when told to move on its own position' do
       subject(:immovable_move) { described_class.new('K0') }
       it "doesn't change its position" do
-        immovable_move.move('K0')
-        expect(immovable_move.position).to be('K0')
+        expect { immovable_move.move('K0') }.to_not change { immovable_move.position }
       end
       it 'returns nil' do
         expect(immovable_move.move('K0')).to be(nil)
@@ -61,8 +105,7 @@ describe Rook do
     context 'when moved diagonally' do
       subject(:diagonal_move) { described_class.new('c4') }
       it "doesn't change its position" do
-        diagonal_move.move('d5')
-        expect(diagonal_move.position).to be('c4')
+        expect { diagonal_move.move('d5') }.to_not change { diagonal_move.position }
       end
       it 'returns nil' do
         expect(diagonal_move.move('b3')).to be(nil)
@@ -71,8 +114,7 @@ describe Rook do
     context 'when told to move on its own position' do
       subject(:immovable_move) { described_class.new('K0') }
       it "doesn't change its position" do
-        immovable_move.move('K0')
-        expect(immovable_move.position).to be('K0')
+        expect { immovable_move.move('K0') }.to_not change { immovable_move.position }
       end
       it 'returns nil' do
         expect(immovable_move.move('K0')).to be(nil)
@@ -93,8 +135,7 @@ describe Bishop do
     context 'when moved vertically' do
       subject(:vertical_move) { described_class.new('a1') }
       it "doesn't change its position" do
-        vertical_move.move('a4')
-        expect(vertical_move.position).to be('a1')
+        expect { vertical_move.move('a4') }.to_not change { vertical_move.position }
       end
       it 'returns nil' do
         expect(vertical_move.move('a6')).to be(nil)
@@ -103,8 +144,7 @@ describe Bishop do
     context 'when moved horizontally' do
       subject(:horizontal_move) { described_class.new('b1') }
       it "doesn't change its position" do
-        horizontal_move.move('h1')
-        expect(horizontal_move.position).to be('b1')
+        expect { horizontal_move.move('h1') }.to_not change { horizontal_move.position }
       end
       it 'returns nil' do
         expect(horizontal_move.move('c1')).to be(nil)
@@ -113,8 +153,7 @@ describe Bishop do
     context 'when told to move on its own position' do
       subject(:immovable_move) { described_class.new('K0') }
       it "doesn't change its position" do
-        immovable_move.move('K0')
-        expect(immovable_move.position).to be('K0')
+        expect { immovable_move.move('K0') }.to_not change { immovable_move.position }
       end
       it 'returns nil' do
         expect(immovable_move.move('K0')).to be(nil)
@@ -122,4 +161,4 @@ describe Bishop do
     end
   end
 end
-# rubocop: enable Layout/LineLength, Metrics/BlockLength
+# rubocop: enable Layout/LineLength, Metrics/BlockLength, Lint/AmbiguousBlockAssociation
