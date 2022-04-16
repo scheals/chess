e125818df5489ddec5556b5896ca280041bd969e
Scheals <piecoo@gmail.com>
2022-04-16 22:04:48 +0200

    Add King move rules

--- lib/king.rb
+++ lib/king.rb
@@ -3,12 +3,16 @@
 require_relative 'piece'
 
 # This class handles a Queen chesspiece.
-class Queen < Piece
+class King < Piece
   attr_reader :position
 
   def legal?(space)
     return false if space == position
 
-    true
+    if (space[0].succ == column || column.succ == space[0]) || (space[1].to_i - row.to_i).abs == 1
+      true
+    else
+      false
+    end
   end
 end
