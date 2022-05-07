ee2e4424aeadfb1ca0bf8726934264390ce8f93a
Scheals <piecoo@gmail.com>
2022-05-07 22:29:34 +0200

    Cleanup testing methods

--- lib/display.rb
+++ lib/display.rb
@@ -17,40 +17,6 @@ module Display
              black_knight: "\u265E",
              black_pawn: "\u265F",
              _piece: "\u2610" }.freeze
-  TILES = { white_tile: ' '.colorize(background: :white),
-            black_tile: ' '.colorize(background: :black) }.freeze
-
-  def test
-    puts PIECES
-    puts TILES
-    print PIECES[:white_king].colorize(background: :light_black).white
-    print PIECES[:white_king].colorize(background: :light_black).black
-    puts PIECES[:black_king].white
-    puts PIECES[:black_king].black
-  end
-
-  # rubocop: disable Metrics/MethodLength
-  def board_test
-    4.times do
-      puts "\n"
-      4.times do
-        print PIECES[:white_pawn].colorize(background: :light_black)
-        print PIECES[:black_pawn]
-      end
-      puts "\n"
-      4.times do
-        print PIECES[:black_pawn]
-        print PIECES[:white_pawn].colorize(background: :light_black)
-      end
-    end
-  end
-  # rubocop: enable Metrics/MethodLength
-
-  def black_tile(square)
-    puts square.colorize(background: :light_black)
-  end
-
-  def white_tile(square)
-    puts square.colorize(background: :light_black)
-  end
+  TILES = { white_tile: :white,
+            black_tile: :light_black }.freeze
 end
