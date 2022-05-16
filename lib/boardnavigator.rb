9778152ca848995890bb465f32c134e3897ddbf9
Scheals <piecoo@gmail.com>
2022-05-16 02:51:42 +0200

    Preemptive refactor of possible moves

--- lib/boardnavigator.rb
+++ lib/boardnavigator.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require_relative 'board'
-
 # This class handles movement logic for a chessboard.
 class BoardNavigator
   attr_reader :board
@@ -10,73 +8,41 @@ class BoardNavigator
     @board = board
   end
 
-  # rubocop: disable all
   def possible_moves(piece)
+    in_bounds = in_bounds_coordinates(piece)
     case piece.class.to_s
     when 'Knight'
-      in_bounds_coordinates(piece) - allied_coordinates(piece)
+      in_bounds - allied_coordinates(piece)
     when 'King'
-      in_bounds_coordinates(piece) - allied_coordinates(piece)
+      in_bounds - allied_coordinates(piece)
     when 'Rook'
-      in_bounds = in_bounds_coordinates(piece)
-      occupied = occupied_coordinates(piece)
-      allies = allied_coordinates(piece)
-      enemies = enemy_coordinates(piece)
-      baseline = in_bounds
-      moves = []
-      horizontal_and_vertical(piece, baseline).each { |direction| moves << enemies_and_allies(piece, direction) }
-      moves_without_allies = []
-      moves.each { |direction| moves_without_allies << remove_allies(direction) }
-      moves_with_legal_enemies = []
-      moves_without_allies.each { |direction| moves_with_legal_enemies << handle_enemies(direction) }
-      enemy_to_coordinate(moves_with_legal_enemies.compact.flatten)
+      handle_collision(piece.split_moves(mark_occupied(piece, in_bounds)))
     else
       []
     end
   end
-# rubocop: enable all
 
-  def enemy_to_coordinate(coordinates)
+  def clean(coordinates)
     coordinates.map { |coordinate| coordinate[0..1] }
   end
 
-  def handle_enemies(direction)
-    direction.slice_after { |coordinate| coordinate.end_with?('E') }.to_a.first
+  def handle_collision(moves)
+    moves_after_collision = moves.map { |direction| handle_allies(direction) }.map { |direction| handle_enemies(direction) }
+    clean(moves_after_collision.compact.flatten)
   end
 
-  def remove_allies(direction)
+  def handle_enemies(direction)
+    direction.slice_after { |coordinate| coordinate.end_with?('E') }.first
+  end
+
+  def handle_allies(direction)
     direction.take_while { |coordinate| !coordinate.end_with?('A') }
   end
 
-  def horizontal_and_vertical(piece, baseline)
-    moves = []
-    moves << up(piece, baseline)
-    moves << down(piece, baseline).reverse
-    moves << left(piece, baseline).reverse
-    moves << right(piece, baseline)
-    moves
-  end
-
-  def up(piece, baseline)
-    baseline.select { |coordinate| coordinate[1].to_i > piece.position[1].to_i }
-  end
-
-  def down(piece, baseline)
-    baseline.select { |coordinate| coordinate[1].to_i < piece.position[1].to_i }
-  end
-
-  def left(piece, baseline)
-    baseline.select { |coordinate| coordinate[0].ord < piece.position[0].ord }
-  end
-
-  def right(piece, baseline)
-    baseline.select { |coordinate| coordinate[0].ord > piece.position[0].ord }
-  end
-
-  def enemies_and_allies(piece, direction)
+  def mark_occupied(piece, coordinates)
     allies = allied_coordinates(piece)
     enemies = enemy_coordinates(piece)
-    direction.map do |coordinate|
+    coordinates.map do |coordinate|
       next "#{coordinate}A" if allies.include?(coordinate)
 
       next "#{coordinate}E" if enemies.include?(coordinate)
@@ -103,6 +69,3 @@ class BoardNavigator
     occupied_coordinates(piece) - allied_coordinates(piece)
   end
 end
-test = BoardNavigator.new(Board.new)
-bishop = Bishop.new('e4', colour: 'white')
-p test.in_bounds_coordinates(bishop)
--- lib/rook.rb
+++ lib/rook.rb
@@ -10,6 +10,31 @@ class Rook < Piece
     horizontal?(space) || vertical?(space)
   end
 
+  def split_moves(moves)
+    split_moves = []
+    split_moves << up(moves)
+    split_moves << down(moves).reverse
+    split_moves << left(moves).reverse
+    split_moves << right(moves)
+    split_moves
+  end
+
+  def up(moves)
+    moves.select { |coordinate| coordinate[1].to_i > position[1].to_i }
+  end
+
+  def down(moves)
+    moves.select { |coordinate| coordinate[1].to_i < position[1].to_i }
+  end
+
+  def left(moves)
+    moves.select { |coordinate| coordinate[0].ord < position[0].ord }
+  end
+
+  def right(moves)
+    moves.select { |coordinate| coordinate[0].ord > position[0].ord }
+  end
+
   def horizontal?(space)
     space[1] == row
   end
