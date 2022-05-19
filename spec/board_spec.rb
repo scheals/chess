4cb76a8ebc86e140a1410609c684cd634fc0d773
Scheals <piecoo@gmail.com>
2022-05-19 21:16:21 +0200

    Autocorrect rubocop offenses

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,14 +3,17 @@
 require_relative '../lib/board'
 require_relative '../lib/square'
 
-# rubocop: disable Metrics/BlockLength,Layout/LineLength, Lint/AmbiguousBlockAssociation
+# rubocop: disable Layout/LineLength, Lint/AmbiguousBlockAssociation
 describe Board do
   describe '#initialize' do
     subject(:new_board) { described_class.new(square) }
+
     let(:square) { class_double(Square) }
+
     before do
       allow(square).to receive(:new).and_return(square)
     end
+
     it 'board is composed of objects resembling squares' do
       expect(new_board.instance_variable_get(:@board).values).to all(be(square))
     end
@@ -18,10 +21,13 @@ describe Board do
 
   describe '#create_piece' do
     subject(:board) { described_class.new }
+
     let(:factory) { board.instance_variable_get(:@factory) }
+
     before do
       allow(factory).to receive(:for).with('Pawn', colour: 'white', position: 'a2')
     end
+
     it 'sends factory a @for message' do
       expect(factory).to receive(:for).with('Pawn', colour: 'white', position: 'a2')
       board.create_piece('Pawn', colour: 'white', position: 'a2')
@@ -31,14 +37,18 @@ describe Board do
   describe '#find' do
     context 'when the square with given coordinates exists' do
       subject(:possible_square) { described_class.new }
+
       let(:board) { possible_square.instance_variable_get(:@board) }
+
       it 'returns the square' do
         a1_square = board['a1']
         expect(possible_square.find('a1')).to be(a1_square)
       end
     end
+
     context 'when the square with given coordinates does not exist' do
       subject(:impossible_square) { described_class.new }
+
       it 'returns nil' do
         expect(impossible_square.find('b20')).to be_nil
       end
@@ -48,22 +58,28 @@ describe Board do
   describe '#put' do
     context 'when square is empty' do
       subject(:empty_board) { described_class.new }
+
       let(:piece) { instance_double(Piece) }
       let(:empty_square) { empty_board.find('d6') }
+
       it 'sends that square a #place message' do
         expect(empty_square).to receive(:place)
         empty_board.put(piece, 'd6')
       end
     end
+
     context 'when square is occupied' do
       subject(:full_board) { described_class.new }
+
       let(:piece) { instance_double(Piece) }
       let(:another_piece) { instance_double(Piece) }
       let(:occupied_square) { full_board.find('a2') }
+
       before do
         occupied_square.place(piece)
         allow(piece).to receive(:real?).and_return(true)
       end
+
       it 'returns nil' do
         expect(full_board.put(piece, 'a2')).to be_nil
       end
@@ -73,13 +89,16 @@ describe Board do
   describe '#in_bounds?' do
     context 'when coordinates are in bound' do
       subject(:in_bounds) { described_class.new }
+
       it 'returns true' do
         ordinary_coordinate = 'f5'
         expect(in_bounds.in_bounds?(ordinary_coordinate)).to be true
       end
     end
+
     context 'when coordinates are not in bound' do
       subject(:not_in_bounds) { described_class.new }
+
       it 'returns false' do
         extraordinary_coordinate = 'Pluto'
         expect(not_in_bounds.in_bounds?(extraordinary_coordinate)).to be false
@@ -90,6 +109,7 @@ describe Board do
   describe '#move_piece' do
     context 'when a square is out of bounds' do
       subject(:out_of_bounds) { described_class.new }
+
       it 'returns nil' do
         reachable_square = 'a1'
         unreachable_square = 'Aquarius'
@@ -100,6 +120,7 @@ describe Board do
 
   describe '#find_piece' do
     subject(:pieceful_board) { described_class.new }
+
     it 'sends Square a piece message' do
       a1_square = pieceful_board.board['a1']
       expect(a1_square).to receive(:piece)
@@ -110,6 +131,7 @@ describe Board do
   describe '#row' do
     context 'when given 2 as an argument' do
       subject(:row_board) { described_class.new }
+
       it 'returns a hash of Squares from the second row' do
         expect(row_board.row(2)).to include(
           'a2' => be_a(Square),
@@ -127,79 +149,99 @@ describe Board do
 
   describe '#setup' do
     subject(:starting_board) { described_class.new }
+
     let(:board) { starting_board.instance_variable_get(:@board) }
+
     before do
       starting_board.setup
     end
+
     it 'puts black Rook into a8' do
       starting_square = board['a8']
       expect(starting_square.piece).to be_a(Rook).and have_attributes(colour: 'black')
     end
+
     it 'puts black Rook into h8' do
       starting_square = board['h8']
       expect(starting_square.piece).to be_a(Rook).and have_attributes(colour: 'black')
     end
+
     it 'puts black Knight into b8' do
       starting_square = board['b8']
       expect(starting_square.piece).to be_a(Knight).and have_attributes(colour: 'black')
     end
+
     it 'puts black Knight into g8' do
       starting_square = board['g8']
       expect(starting_square.piece).to be_a(Knight).and have_attributes(colour: 'black')
     end
+
     it 'puts black Bishop into c8' do
       starting_square = board['c8']
       expect(starting_square.piece).to be_a(Bishop).and have_attributes(colour: 'black')
     end
+
     it 'puts black Bishop into f8' do
       starting_square = board['f8']
       expect(starting_square.piece).to be_a(Bishop).and have_attributes(colour: 'black')
     end
+
     it 'puts black Queen into d8' do
       starting_square = board['d8']
       expect(starting_square.piece).to be_a(Queen).and have_attributes(colour: 'black')
     end
+
     it 'puts black King into e8' do
       starting_square = board['e8']
       expect(starting_square.piece).to be_a(King).and have_attributes(colour: 'black')
     end
+
     it 'puts black Pawns into row 7' do
       starting_row_squares = starting_board.row(7).values
       starting_row_pieces = starting_row_squares.map(&:piece)
       expect(starting_row_pieces).to all(be_a(Pawn).and(have_attributes(colour: 'black')))
     end
+
     it 'puts white Rook into a1' do
       starting_square = board['a1']
       expect(starting_square.piece).to be_a(Rook).and have_attributes(colour: 'white')
     end
+
     it 'puts white Rook into h1' do
       starting_square = board['h1']
       expect(starting_square.piece).to be_a(Rook).and have_attributes(colour: 'white')
     end
+
     it 'puts white Knight into b1' do
       starting_square = board['b1']
       expect(starting_square.piece).to be_a(Knight).and have_attributes(colour: 'white')
     end
+
     it 'puts white Knight into g1' do
       starting_square = board['g1']
       expect(starting_square.piece).to be_a(Knight).and have_attributes(colour: 'white')
     end
+
     it 'puts white Bishop into c1' do
       starting_square = board['c1']
       expect(starting_square.piece).to be_a(Bishop).and have_attributes(colour: 'white')
     end
+
     it 'puts white Bishop into f1' do
       starting_square = board['f1']
       expect(starting_square.piece).to be_a(Bishop).and have_attributes(colour: 'white')
     end
+
     it 'puts white Queen into d1' do
       starting_square = board['d1']
       expect(starting_square.piece).to be_a(Queen).and have_attributes(colour: 'white')
     end
+
     it 'puts white King into e1' do
       starting_square = board['e1']
       expect(starting_square.piece).to be_a(King).and have_attributes(colour: 'white')
     end
+
     it 'puts white Pawns into row 2' do
       starting_row_squares = starting_board.row(2).values
       starting_row_pieces = starting_row_squares.map(&:piece)
@@ -212,22 +254,29 @@ describe Square do
   describe '#place' do
     context 'when square is not occupied by a piece' do
       subject(:empty_square) { described_class.new('c4') }
+
       let(:piece) { instance_double(Piece) }
+
       it 'stores the piece in @piece' do
         expect { empty_square.place(piece) }.to change { empty_square.instance_variable_get(:@piece) }
       end
     end
+
     context 'when square is occupied by a piece' do
       subject(:full_square) { described_class.new('d8') }
+
       let(:piece) { instance_double(Piece) }
       let(:another_piece) { instance_double(Piece) }
+
       before do
         full_square.place(piece)
         allow(piece).to receive(:real?).and_return(true)
       end
+
       it 'does not change @piece' do
-        expect { full_square.place(another_piece) }.to_not change { full_square.instance_variable_get(:@piece) }
+        expect { full_square.place(another_piece) }.not_to change { full_square.instance_variable_get(:@piece) }
       end
+
       it 'returns nil' do
         expect(full_square.place(another_piece)).to be_nil
       end
@@ -237,17 +286,22 @@ describe Square do
   describe '#vacate' do
     context 'when square is occupied by a piece' do
       subject(:occupied_square) { described_class.new('a1') }
+
       let(:piece) { instance_double(Piece) }
+
       before do
         occupied_square.place(piece)
         allow(piece).to receive(:real?).and_return(true)
       end
+
       it 'changes @piece into a NilPiece' do
         expect { occupied_square.vacate }.to change { occupied_square.instance_variable_get(:@piece) }.from(piece).to be_a(NilPiece)
       end
     end
+
     context 'when square is empty' do
       subject(:empty_square) { described_class.new('h4') }
+
       it 'returns nil' do
         expect(empty_square.vacate).to be_nil
       end
@@ -257,21 +311,26 @@ describe Square do
   describe '#occupied?' do
     context 'when square is occupied by a piece' do
       subject(:pieceful_square) { described_class.new('a6') }
+
       let(:piece) { instance_double(Piece) }
+
       before do
         pieceful_square.place(piece)
         allow(piece).to receive(:real?).and_return(true)
       end
+
       it 'returns true' do
         expect(pieceful_square.occupied?).to be(true)
       end
     end
+
     context 'when square is empty' do
       subject(:pieceless_square) { described_class.new('g6') }
+
       it 'returns false' do
         expect(pieceless_square.occupied?).to be(false)
       end
     end
   end
 end
-# rubocop: enable Metrics/BlockLength, Layout/LineLength, Lint/AmbiguousBlockAssociation
+# rubocop: enable Layout/LineLength, Lint/AmbiguousBlockAssociation
--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -10,10 +10,11 @@ require_relative '../lib/pawn'
 require_relative '../lib/nilpiece'
 require_relative '../lib/piecefactory'
 
-# rubocop: disable Metrics/BlockLength, Lint/AmbiguousBlockAssociation, Layout/LineLength
+# rubocop: disable Lint/AmbiguousBlockAssociation, Layout/LineLength
 describe Piece do
   describe 'real?' do
     subject(:real_piece) { described_class.new('a1') }
+
     it 'always returns true' do
       expect(real_piece.real?).to be true
     end
@@ -23,6 +24,7 @@ end
 describe NilPiece do
   describe 'real?' do
     subject(:false_piece) { described_class.new('a1') }
+
     it 'always returns false' do
       expect(false_piece.real?).to be false
     end
@@ -33,47 +35,60 @@ describe King do
   describe '#move' do
     context 'when moved vertically by one tile' do
       subject(:vertical_move) { described_class.new('a1') }
+
       it 'changes its position' do
         vertical_move.move('a2')
         expect(vertical_move.position).to be('a2')
       end
+
       it 'does not move further than one tile' do
-        expect { vertical_move.move('a6') }.to_not change { vertical_move.position }
+        expect { vertical_move.move('a6') }.not_to change { vertical_move.position }
       end
     end
+
     context 'when moved horizontally by one tile' do
       subject(:horizontal_move) { described_class.new('b1') }
+
       it 'changes its position' do
         horizontal_move.move('c1')
         expect(horizontal_move.position).to be('c1')
       end
+
       it 'does not move further than one tile' do
-        expect { horizontal_move.move('h1') }.to_not change { horizontal_move.position }
+        expect { horizontal_move.move('h1') }.not_to change { horizontal_move.position }
       end
     end
+
     context 'when moved diagonally by one tile' do
       subject(:diagonal_move) { described_class.new('c4') }
+
       it 'changes its position' do
         diagonal_move.move('d5')
         expect(diagonal_move.position).to be('d5')
       end
+
       it 'does not move further than one tile' do
-        expect { diagonal_move.move('e6') }.to_not change { diagonal_move.position }
+        expect { diagonal_move.move('e6') }.not_to change { diagonal_move.position }
       end
     end
+
     context 'when told to make an illegal move' do
       subject(:illegal_move) { described_class.new('c5') }
+
       it 'does not change its position' do
-        expect { illegal_move.move('a6') }.to_not change { illegal_move.position }
+        expect { illegal_move.move('a6') }.not_to change { illegal_move.position }
       end
     end
+
     context 'when told to move on its own position' do
       subject(:immovable_move) { described_class.new('K0') }
+
       it "doesn't change its position" do
-        expect { immovable_move.move('K0') }.to_not change { immovable_move.position }
+        expect { immovable_move.move('K0') }.not_to change { immovable_move.position }
       end
+
       it 'returns nil' do
-        expect(immovable_move.move('K0')).to be(nil)
+        expect(immovable_move.move('K0')).to be_nil
       end
     end
   end
@@ -83,41 +98,52 @@ describe Queen do
   describe '#move' do
     context 'when moved vertically' do
       subject(:vertical_move) { described_class.new('a1') }
+
       it 'changes its position' do
         vertical_move.move('a4')
         expect(vertical_move.position).to be('a4')
       end
     end
+
     context 'when moved horizontally' do
       subject(:horizontal_move) { described_class.new('b1') }
+
       it 'changes its position' do
         horizontal_move.move('h1')
         expect(horizontal_move.position).to be('h1')
       end
     end
+
     context 'when moved diagonally' do
       subject(:diagonal_move) { described_class.new('c4') }
+
       it 'changes its position' do
         diagonal_move.move('d5')
         expect(diagonal_move.position).to be('d5')
       end
     end
+
     context 'when told to make an invalid move' do
       subject(:invalid_move) { described_class.new('d4') }
+
       it "doesn't change its position" do
-        expect { invalid_move.move('c7') }.to_not change { invalid_move.position }
+        expect { invalid_move.move('c7') }.not_to change { invalid_move.position }
       end
+
       it 'returns nil' do
         expect(invalid_move.move('c7')).to be_nil
       end
     end
+
     context 'when told to move on its own position' do
       subject(:immovable_move) { described_class.new('K0') }
+
       it "doesn't change its position" do
-        expect { immovable_move.move('K0') }.to_not change { immovable_move.position }
+        expect { immovable_move.move('K0') }.not_to change { immovable_move.position }
       end
+
       it 'returns nil' do
-        expect(immovable_move.move('K0')).to be(nil)
+        expect(immovable_move.move('K0')).to be_nil
       end
     end
   end
@@ -127,43 +153,55 @@ describe Rook do
   describe '#move' do
     context 'when moved vertically' do
       subject(:vertical_move) { described_class.new('a1') }
+
       it 'changes its position' do
         vertical_move.move('a4')
         expect(vertical_move.position).to be('a4')
       end
     end
+
     context 'when moved horizontally' do
       subject(:horizontal_move) { described_class.new('b1') }
+
       it 'changes its position' do
         horizontal_move.move('h1')
         expect(horizontal_move.position).to be('h1')
       end
     end
+
     context 'when moved diagonally' do
       subject(:diagonal_move) { described_class.new('c4') }
+
       it "doesn't change its position" do
-        expect { diagonal_move.move('d5') }.to_not change { diagonal_move.position }
+        expect { diagonal_move.move('d5') }.not_to change { diagonal_move.position }
       end
+
       it 'returns nil' do
-        expect(diagonal_move.move('b3')).to be(nil)
+        expect(diagonal_move.move('b3')).to be_nil
       end
     end
+
     context 'when told to make an invalid move' do
       subject(:invalid_move) { described_class.new('d4') }
+
       it "doesn't change its position" do
-        expect { invalid_move.move('c7') }.to_not change { invalid_move.position }
+        expect { invalid_move.move('c7') }.not_to change { invalid_move.position }
       end
+
       it 'returns nil' do
         expect(invalid_move.move('c7')).to be_nil
       end
     end
+
     context 'when told to move on its own position' do
       subject(:immovable_move) { described_class.new('K0') }
+
       it "doesn't change its position" do
-        expect { immovable_move.move('K0') }.to_not change { immovable_move.position }
+        expect { immovable_move.move('K0') }.not_to change { immovable_move.position }
       end
+
       it 'returns nil' do
-        expect(immovable_move.move('K0')).to be(nil)
+        expect(immovable_move.move('K0')).to be_nil
       end
     end
   end
@@ -173,45 +211,58 @@ describe Bishop do
   describe '#move' do
     context 'when moved diagonally' do
       subject(:diagonal_move) { described_class.new('c4') }
+
       it 'changes its position' do
         diagonal_move.move('d5')
         expect(diagonal_move.position).to be('d5')
       end
     end
+
     context 'when moved vertically' do
       subject(:vertical_move) { described_class.new('a1') }
+
       it "doesn't change its position" do
-        expect { vertical_move.move('a4') }.to_not change { vertical_move.position }
+        expect { vertical_move.move('a4') }.not_to change { vertical_move.position }
       end
+
       it 'returns nil' do
-        expect(vertical_move.move('a6')).to be(nil)
+        expect(vertical_move.move('a6')).to be_nil
       end
     end
+
     context 'when moved horizontally' do
       subject(:horizontal_move) { described_class.new('b1') }
+
       it "doesn't change its position" do
-        expect { horizontal_move.move('h1') }.to_not change { horizontal_move.position }
+        expect { horizontal_move.move('h1') }.not_to change { horizontal_move.position }
       end
+
       it 'returns nil' do
-        expect(horizontal_move.move('c1')).to be(nil)
+        expect(horizontal_move.move('c1')).to be_nil
       end
     end
+
     context 'when told to make an invalid move' do
       subject(:invalid_move) { described_class.new('d4') }
+
       it "doesn't change its position" do
-        expect { invalid_move.move('c7') }.to_not change { invalid_move.position }
+        expect { invalid_move.move('c7') }.not_to change { invalid_move.position }
       end
+
       it 'returns nil' do
         expect(invalid_move.move('c7')).to be_nil
       end
     end
+
     context 'when told to move on its own position' do
       subject(:immovable_move) { described_class.new('K0') }
+
       it "doesn't change its position" do
-        expect { immovable_move.move('K0') }.to_not change { immovable_move.position }
+        expect { immovable_move.move('K0') }.not_to change { immovable_move.position }
       end
+
       it 'returns nil' do
-        expect(immovable_move.move('K0')).to be(nil)
+        expect(immovable_move.move('K0')).to be_nil
       end
     end
   end
@@ -220,61 +271,72 @@ end
 describe Knight do
   describe '#move' do
     subject(:centre_position) { described_class.new('d4') }
+
     context 'when moved twice left then up once' do
       it 'changes its position' do
         centre_position.move('b5')
         expect(centre_position.position).to be('b5')
       end
     end
+
     context 'when moved twice left then down once' do
       it 'changes its position' do
         centre_position.move('b3')
         expect(centre_position.position).to be('b3')
       end
     end
+
     context 'when moved twice right then up once' do
       it 'changes its position' do
         centre_position.move('f5')
         expect(centre_position.position).to be('f5')
       end
     end
+
     context 'when moved twice right then down once' do
       it 'changes its position' do
         centre_position.move('f3')
         expect(centre_position.position).to be('f3')
       end
     end
+
     context 'when moved twice up then left once' do
       it 'changes its position' do
         centre_position.move('c6')
         expect(centre_position.position).to be('c6')
       end
     end
+
     context 'when moved twice up then right once' do
       it 'changes its position' do
         centre_position.move('e6')
         expect(centre_position.position).to be('e6')
       end
     end
+
     context 'when moved twice down then left once' do
       it 'changes its position' do
         centre_position.move('c2')
         expect(centre_position.position).to be('c2')
       end
     end
+
     context 'when moved twice down then right once' do
       it 'changes its position' do
         centre_position.move('e2')
         expect(centre_position.position).to be('e2')
       end
     end
+
     context 'when told to move on its own position' do
       subject(:immovable_move) { described_class.new('K0') }
+
       it "doesn't change its position" do
-        expect { immovable_move.move('K0') }.to_not change { immovable_move.position }
+        expect { immovable_move.move('K0') }.not_to change { immovable_move.position }
       end
+
       it 'returns nil' do
-        expect(immovable_move.move('K0')).to be(nil)
+        expect(immovable_move.move('K0')).to be_nil
       end
     end
   end
@@ -284,25 +346,33 @@ describe Pawn do
   describe '#move' do
     context 'when Pawn is white' do
       subject(:white_pawn) { described_class.new('c3', colour: 'white') }
+
       it 'can only go up one space at a time' do
         white_pawn.move('c4')
         expect(white_pawn.position).to be('c4')
       end
+
       it 'does not make an illegal move' do
-        expect { white_pawn.move('d5') }.to_not change { white_pawn.position }
+        expect { white_pawn.move('d5') }.not_to change { white_pawn.position }
       end
+
       context 'when it has not moved yet' do
         subject(:white_double) { described_class.new('b2', colour: 'white') }
+
         it 'can move two spaces up at once' do
           expect { white_double.move('b4') }.to change { white_double.position }.from('b2').to('b4')
         end
       end
+
       context 'when there is a piece to be taken diagonally' do
         subject(:white_take) { described_class.new('f4', colour: 'white') }
+
         let(:occupied_space) { double('Space', coordinates: 'e5') }
+
         before do
           allow(occupied_space).to receive(:occupied?).and_return(true)
         end
+
         it 'can move one space up diagonally' do
           space = occupied_space.coordinates
           white_take.move(space)
@@ -310,27 +380,36 @@ describe Pawn do
         end
       end
     end
+
     context 'when Pawn is black' do
       subject(:black_pawn) { described_class.new('e5', colour: 'black') }
+
       it 'can only go down one space at a time' do
         black_pawn.move('e4')
         expect(black_pawn.position).to be('e4')
       end
+
       it 'does not make an illegal move' do
-        expect { black_pawn.move('f3') }.to_not change { black_pawn.position }
+        expect { black_pawn.move('f3') }.not_to change { black_pawn.position }
       end
+
       context 'when it has not moved yet' do
         subject(:black_double) { described_class.new('e7', colour: 'black') }
+
         it 'can move two spaces down at once' do
           expect { black_double.move('e5') }.to change { black_double.position }.from('e7').to('e5')
         end
       end
+
       context 'when there is a piece to be taken diagonally' do
         subject(:black_take) { described_class.new('c5', colour: 'black') }
+
         let(:occupied_space) { double('Space', coordinates: 'd4') }
+
         before do
           allow(occupied_space).to receive(:occupied?).and_return(true)
         end
+
         it 'can move one space down diagonally' do
           space = occupied_space.coordinates
           black_take.move(space)
@@ -346,24 +425,31 @@ describe PieceFactory do
     it 'creates a Pawn instance' do
       expect(PieceFactory.for('Pawn', position: 'a2')).to be_a(Pawn)
     end
+
     it 'creates a Rook instance' do
       expect(PieceFactory.for('Rook', position: 'a1')).to be_a(Rook)
     end
+
     it 'creates a Knight instance' do
       expect(PieceFactory.for('Knight', position: 'b1')).to be_a(Knight)
     end
+
     it 'creates a Bishop instance' do
       expect(PieceFactory.for('Bishop', position: 'c1')).to be_a(Bishop)
     end
+
     it 'creates a Queen instance' do
       expect(PieceFactory.for('Queen', position: 'd1')).to be_a(Queen)
     end
+
     it 'creates a King instance' do
       expect(PieceFactory.for('King', position: 'e1')).to be_a(King)
     end
+
     it 'defaults to a Piece instance' do
       expect(PieceFactory.for('Unicorn', position: 'L4')).to be_a(Piece)
     end
+
     context 'when given a colour' do
       it 'creates a piece with the right colour' do
         piece = 'Pawn'
@@ -372,4 +458,4 @@ describe PieceFactory do
     end
   end
 end
-# rubocop: enable Metrics/BlockLength, Lint/AmbiguousBlockAssociation, Layout/LineLength
+# rubocop: enable Lint/AmbiguousBlockAssociation, Layout/LineLength
