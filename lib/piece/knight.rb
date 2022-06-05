9c61578076000c5512d2b094293350ad0ea70aa2
scheals <piecoo@gmail.com>
2022-06-06 00:52:55 +0200

    Make Knight use Coordinates

--- lib/piece/knight.rb
+++ lib/piece/knight.rb
@@ -5,28 +5,30 @@ require_relative '../piece'
 # This class handles a Knight chesspiece rules.
 class Knight < Piece
   def legal?(space)
-    return false if space == position
+    to_coordinate = Coordinate.parse(space)
+    current_coordinate = Coordinate.parse(position)
+    return false if current_coordinate == to_coordinate
 
-    (twice_vertical?(space) && once_horizontal?(space)) || (twice_horizontal?(space) && once_vertical?(space))
+    (twice_vertical?(to_coordinate) && once_horizontal?(to_coordinate)) || (twice_horizontal?(to_coordinate) && once_vertical?(to_coordinate))
   end
 
   def split_moves(moves)
     moves.map { |coordinate| [coordinate] }
   end
 
-  def twice_vertical?(space)
-    (space[1].to_i - row.to_i).abs == 2
+  def twice_vertical?(coordinate)
+    row == coordinate.up.up.row || row == coordinate.down.down.row
   end
 
-  def once_vertical?(space)
-    (space[1].to_i - row.to_i).abs == 1
+  def once_vertical?(coordinate)
+    row == coordinate.up.row || row == coordinate.down.row
   end
 
-  def twice_horizontal?(space)
-    space[0].succ.succ == column || column.succ.succ == space[0]
+  def twice_horizontal?(coordinate)
+    coordinate.right.right.column == column || coordinate.left.left.column == column
   end
 
-  def once_horizontal?(space)
-    space[0].succ == column || column.succ == space[0]
+  def once_horizontal?(coordinate)
+    coordinate.right.column == column || coordinate.left.column == column
   end
 end
