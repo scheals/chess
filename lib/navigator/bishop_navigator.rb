080fd6616c34ce9a0560aa09d7c212ba126b964b
Scheals <piecoo@gmail.com>
2022-06-19 01:53:27 +0200

    Refactor possible_moves

--- lib/navigator/bishop_navigator.rb
+++ lib/navigator/bishop_navigator.rb
@@ -7,7 +7,62 @@ class BishopNavigator < PieceNavigator
   attr_reader :piece
 
   def possible_moves
-    coordinates = legal_for(piece)
-    handle_collision(piece.split_moves(coordinates)).compact.flatten
+    go_up_left + go_up_right + go_down_left + go_down_right
+  end
+
+  def go_up_left
+    queue = [piece.position.up.left]
+    moves = []
+    until queue.empty?
+      move = queue.shift
+      current_square = board.find(move.to_s)
+      break unless current_square
+
+      moves << move if empty_or_enemy?(current_square)
+      queue.push(move.up.left) if passable?(move.up.left.to_s, current_square)
+    end
+    moves
+  end
+
+  def go_up_right
+    queue = [piece.position.up.right]
+    moves = []
+    until queue.empty?
+      move = queue.shift
+      current_square = board.find(move.to_s)
+      break unless current_square
+
+      moves << move if empty_or_enemy?(current_square)
+      queue.push(move.up.right) if passable?(move.up.right.to_s, current_square)
+    end
+    moves
+  end
+
+  def go_down_left
+    queue = [piece.position.down.left]
+    moves = []
+    until queue.empty?
+      move = queue.shift
+      current_square = board.find(move.to_s)
+      break unless current_square
+
+      moves << move if empty_or_enemy?(current_square)
+      queue.push(move.down.left) if passable?(move.down.left.to_s, current_square)
+    end
+    moves
+  end
+
+  def go_down_right
+    queue = [piece.position.down.right]
+    moves = []
+    until queue.empty?
+      move = queue.shift
+      current_square = board.find(move.to_s)
+      break unless current_square
+
+      moves << move if empty_or_enemy?(current_square)
+      queue.push(move.down.right) if passable?(move.down.right.to_s, current_square)
+    end
+    moves
   end
 end
