f65ee08871d59f01b8151ab0b2108ac29e14def3
Scheals <piecoo@gmail.com>
2022-08-14 23:45:18 +0200

    Add move validation

--- lib/game.rb
+++ lib/game.rb
@@ -2,17 +2,18 @@
 
 require_relative './player'
 require_relative './move'
+require_relative './coordinate'
 
 # This class handles a game of Chess.
 class Game
   attr_accessor :current_player
   attr_reader :board_navigator
 
-  def initialize(player1 = Player.new, player2 = Player.new, board_navigator = BoardNavigator.new(Board.new))
+  def initialize(player1 = Player.new('White', 'white'), player2 = Player.new('Black', 'black'), board_navigator = BoardNavigator.new(Board.new))
     @board_navigator = board_navigator
     @player1 = player1
     @player2 = player2
-    @current_player = nil
+    @current_player = player1
   end
 
   def pick_piece(coordinate)
@@ -24,5 +25,62 @@ class Game
 
   def create_move(coordinate, move_system = Move)
     move_system.parse(coordinate)
+  end
+
+  def ask_for_move
+    unvalidated_move = nil
+    loop do
+      puts 'Enter a full move like \'a8c8\' or a partial move like \'a8\' to see possible moves of that piece.'
+      unvalidated_move = Move.parse(gets.chomp.downcase)
+      break if unvalidated_move
+
+      puts 'Your move is either too long or too short in length.'
+    end
+
+    return nil unless in_bounds?(unvalidated_move) && current_player_owns?(unvalidated_move.start)
+
+    validate_target(unvalidated_move)
+  end
+
+  def validate_target(move)
+    if legal_target?(move)
+      move
+    else
+      ask_for_target(move)
+    end
+  end
+
+  def ask_for_target(move)
+    possible_moves = board_navigator.moves_for(move.start)
+    loop do
+      # chess_display.highlight_moves(possible_moves)
+      puts "Those are your possible moves: #{possible_moves}"
+      puts 'Which one do you want to make? Type \'q\' if you want to restart making your move.'
+      completed_move = Move.new(move.start, gets.chomp.downcase)
+      break nil if completed_move.target == 'q'
+      break completed_move if legal_target?(completed_move)
+    end
+  end
+
+  def in_bounds?(move)
+    return true if move.in_bounds?(board_navigator.board)
+
+    puts 'Your move is out of bounds.'
+    false
+  end
+
+  def current_player_owns?(coordinate)
+    return true if pick_piece(coordinate)
+
+    puts "You do not own piece at #{coordinate}."
+    false
+  end
+
+  def legal_target?(move)
+    return true if board_navigator.moves_for(move.start).include?(Coordinate.parse(move.target))
+
+    puts 'Your move lacks a target.' unless move.target
+    puts 'This move can not be made by this piece.' if move.target
+    false
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -60,4 +60,218 @@ describe Game do
       expect(move_system).to have_received(:parse).with(coordinate)
     end
   end
+
+  describe '#ask_for_move' do
+    # This test stubs a lot, coupling it tightly to the current implementation. Sadly.
+    context 'when move is complete and valid' do
+      subject(:good_input) { described_class.new }
+
+      let(:proper_move) { 'a4b5' }
+
+      before do
+        allow(good_input).to receive(:gets).and_return(proper_move)
+        allow(good_input).to receive(:in_bounds?).with(Move.parse(proper_move)).and_return(true)
+        allow(good_input).to receive(:current_player_owns?).with((Move.parse(proper_move)).start).and_return(true)
+        allow(good_input).to receive(:legal_target?).with(Move.parse(proper_move)).and_return(true)
+      end
+
+      it 'returns that move' do
+        expect(good_input.ask_for_move).to eq(Move.parse(proper_move))
+      end
+    end
+
+    context 'when move is complete but not valid' do
+      subject(:out_of_bounds_input) { described_class.new }
+
+      let(:improper_move) { 'a9k3' }
+
+      before do
+        allow(out_of_bounds_input).to receive(:gets).and_return(improper_move)
+        allow(out_of_bounds_input).to receive(:in_bounds?).with(Move.parse(improper_move)).and_return(false)
+      end
+
+      it 'does not return that move' do
+        expect(out_of_bounds_input.ask_for_move).not_to eq(Move.parse(improper_move))
+      end
+
+      it 'returns nil' do
+        expect(out_of_bounds_input.ask_for_move).to be_nil
+      end
+    end
+
+    context 'when move is incomplete' do
+      subject(:incomplete_input) { described_class.new }
+
+      let(:half_move) { 'c3' }
+
+      before do
+        allow(incomplete_input).to receive(:gets).and_return(half_move)
+      end
+
+      xit 'does not return that move' do
+        expect(incomplete_input.ask_for_move).not_to eq(Move.parse(half_move))
+      end
+    end
+  end
+
+  describe '#in_bounds?' do
+    context 'when the full move is in bounds' do
+      subject(:game_in_bound_move) { described_class.new(player, player, board_navigator) }
+
+      let(:player) { instance_double(Player) }
+      let(:board_navigator) { instance_double(BoardNavigator, board:) }
+      let(:board) { instance_double(Board) }
+      let(:in_bounds_move) { Move.parse('a1b1') }
+
+      before do
+        allow(board).to receive(:in_bounds?).with(in_bounds_move.start).and_return(true)
+        allow(board).to receive(:in_bounds?).with(in_bounds_move.target).and_return(true)
+      end
+
+      it 'returns true' do
+        expect(game_in_bound_move.in_bounds?(in_bounds_move)).to be true
+      end
+    end
+
+    context 'when the partial move is not in bounds' do
+      subject(:game_out_of_bound_move) { described_class.new(player, player, board_navigator) }
+
+      let(:player) { instance_double(Player) }
+      let(:board_navigator) { instance_double(BoardNavigator, board:) }
+      let(:board) { instance_double(Board) }
+      let(:out_of_bounds_move) { Move.parse('a9') }
+
+      before do
+        allow(board).to receive(:in_bounds?).with(out_of_bounds_move.start).and_return(false)
+      end
+
+      it 'returns false' do
+        expect(game_out_of_bound_move.in_bounds?(out_of_bounds_move)).to be false
+      end
+    end
+  end
+
+  describe '#current_player_owns?' do
+    context 'when the current player owns the piece' do
+      subject(:game_current_player_owns) { described_class.new(player, player, board_navigator)}
+
+      let(:player) { instance_double(Player, colour: 'white') }
+      let(:board_navigator) { instance_double(BoardNavigator) }
+      let(:move) { Move.parse('a3b3') }
+      let(:piece) { instance_double(Piece, colour: 'white') }
+
+      before do
+        game_current_player_owns.current_player = player
+        allow(board_navigator).to receive(:piece_for).with(move.start).and_return(piece)
+      end
+
+      it 'returns true' do
+        expect(game_current_player_owns.current_player_owns?(move.start)).to be true
+      end
+    end
+
+    context 'when the current players does not own the piece' do
+      subject(:game_current_player_does_not_own) { described_class.new(player, player, board_navigator)}
+
+      let(:player) { instance_double(Player, colour: 'black') }
+      let(:board_navigator) { instance_double(BoardNavigator) }
+      let(:move) { Move.parse('a6d6') }
+      let(:piece) { instance_double(Piece, colour: 'white') }
+
+      before do
+        game_current_player_does_not_own.instance_variable_set(:@current_player, player)
+        allow(board_navigator).to receive(:piece_for).with(move.start).and_return(piece)
+      end
+
+      it 'returns false' do
+        expect(game_current_player_does_not_own.current_player_owns?(move.start)).to be false
+      end
+    end
+  end
+
+  describe '#legal_target?' do
+    context 'when target is legal' do
+      subject(:game_legal_target) { described_class.new(player, player, board_navigator)}
+
+      let(:player) { instance_double(Player, colour: 'black') }
+      let(:board_navigator) { instance_double(BoardNavigator) }
+      let(:move) { Move.parse('a6d6') }
+
+      before do
+        allow(board_navigator).to receive(:moves_for).with(move.start).and_return([Coordinate.parse('d6')])
+      end
+
+      it 'returns true' do
+        expect(game_legal_target.legal_target?(move)).to be true
+      end
+    end
+
+    context 'when target is illegal' do
+      subject(:game_illegal_target) { described_class.new(player, player, board_navigator)}
+
+      let(:player) { instance_double(Player, colour: 'black') }
+      let(:board_navigator) { instance_double(BoardNavigator) }
+      let(:move) { Move.parse('b7h2') }
+
+      before do
+        allow(board_navigator).to receive(:moves_for).with(move.start).and_return(['d6', 'd3'])
+      end
+
+      it 'returns false' do
+        expect(game_illegal_target.legal_target?(move)).to be false
+      end
+    end
+  end
+
+  describe '#validate_target' do
+    context 'when target is legal' do
+      subject(:game_valid_move) { described_class.new }
+
+      let(:move) { Move.parse('b7h7') }
+
+      before do
+        game_valid_move.board_navigator.board.setup('k7/1r6/8/8/8/8/8/K7')
+      end
+
+      it 'returns the move' do
+        expect(game_valid_move.validate_target(move)).to be move
+      end
+    end
+
+    context 'when target is not legal' do
+      context 'when move is completed afterwards' do
+        subject(:game_target_complete) { described_class.new(player, player, board_navigator) }
+
+        let(:player) { instance_double(Player) }
+        let(:board_navigator) { instance_double(BoardNavigator) }
+        let(:move) { Move.parse('b7h9') }
+
+        before do
+          allow(game_target_complete).to receive(:gets).and_return('b8')
+          allow(board_navigator).to receive(:moves_for).with(move.start).and_return([Coordinate.parse('b8')])
+        end
+
+        it 'returns that completed move' do
+          expect(game_target_complete.validate_target(move)).to eq(Move.parse("#{move.start}b8"))
+        end
+      end
+
+      context 'when \'q\' is input' do
+        subject(:game_target_quit) { described_class.new(player, player, board_navigator) }
+
+        let(:player) { instance_double(Player) }
+        let(:board_navigator) { instance_double(BoardNavigator) }
+        let(:move) { Move.parse('b7h9') }
+
+        before do
+          allow(game_target_quit).to receive(:gets).and_return('q')
+          allow(board_navigator).to receive(:moves_for).with(move.start).and_return([Coordinate.parse('b8')])
+        end
+
+        it 'returns nil' do
+          expect(game_target_quit.validate_target(move)).to be_nil
+        end
+      end
+    end
+  end
 end
