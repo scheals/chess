56e59ab549bf7fb02585a854d183b01a0d4fccfd
Scheals <95084627+scheals@users.noreply.github.com>
2022-09-11 21:46:56 +0200

    Add threefold repetition test

--- spec/e2e/game_gameloop_spec.rb
+++ spec/e2e/game_gameloop_spec.rb
@@ -4,7 +4,7 @@ require_relative '../../lib/game'
 require_relative '../../lib/board_navigator'
 require_relative '../../lib/board'
 
-xdescribe Game do
+describe Game do
   describe '#game_loop' do
     context 'when promotion ends the game' do
       subject(:promoted_to_win) { described_class.new }
@@ -59,6 +59,29 @@ xdescribe Game do
 
       it 'correctly reflects that by ending the loop' do
         expect { en_passant_finish.game_loop }.not_to change(en_passant_finish, :current_player)
+      end
+    end
+
+    context 'when threefold repetition ends the game' do
+      subject(:threefold_finish) { described_class.new }
+
+      before do
+        portisch_korchnoi_1970 = ['g1f3', 'c7c5', 'c2c4', 'g8f6', 'b1c3', 'b8c6', 'd2d4', 'c5d4', 'f3d4', 'e7e6', 'g2g3', 'd8b6',
+                                  'd4b3', 'c6e5', 'e2e4', 'f8b4', 'd1e2', 'e8g8',  'f2f4', 'e5c6', 'e4e5', 'f6e8', 'c1d2',
+                                  'f7f6', 'c4c5', 'b6d8', 'a2a3', 'b4c3', 'd2c3', 'f6e5', 'c3e5', 'b7b6', 'f1g2',
+                                  'c6e5', 'g2a8', 'e5f7', 'a8g2', 'b6c5', 'b3c5', 'd8b6',  'e2f2', 'b6b5',
+                                  'g2f1', 'b5c6', 'f1g2', 'c6b5', 'g2f1', 'b5c6', 'f1g2', 'c6b5']
+        threefold_finish.board_navigator.board.setup
+        allow(threefold_finish).to receive(:gets).and_return('g1f3', 'c7c5', 'c2c4', 'g8f6', 'b1c3', 'b8c6', 'd2d4', 'c5d4', 'f3d4', 'e7e6', 'g2g3', 'd8b6',
+                                                             'd4b3', 'c6e5', 'e2e4', 'f8b4', 'd1e2', 'e8g8', 'f2f4', 'e5c6', 'e4e5', 'f6e8', 'c1d2',
+                                                             'f7f6', 'c4c5', 'b6d8', 'a2a3', 'b4c3', 'd2c3', 'f6e5', 'c3e5', 'b7b6', 'f1g2',
+                                                             'c6e5', 'g2a8', 'e5f7', 'a8g2', 'b6c5', 'b3c5', 'd8b6', 'e2f2', 'b6b5',
+                                                             'g2f1', 'b5c6', 'f1g2', 'c6b5', 'g2f1', 'b5c6', 'f1g2', 'c6b5')
+        allow(threefold_finish).to receive(:puts)
+      end
+
+      it 'correctly reflects that by ending the loop' do
+        expect { threefold_finish.game_loop }.to change(threefold_finish, :current_player)
       end
     end
   end
