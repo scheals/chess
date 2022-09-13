e06493b5e0923b55fd64c1e752c6d5c921721e64
Scheals <95084627+scheals@users.noreply.github.com>
2022-09-13 20:29:21 +0200

    Fix wrong parameter order

--- lib/display.rb
+++ lib/display.rb
@@ -3,20 +3,6 @@
 # rubocop: disable Metrics/ModuleLength
 # This module handles a console display.
 module Display
-  PIECES = { white_king: "\u2654",
-             white_queen: "\u2655",
-             white_rook: "\u2656",
-             white_bishop: "\u2657",
-             white_knight: "\u2658",
-             white_pawn: "\u2659",
-             black_king: "\u265A",
-             black_queen: "\u265B",
-             black_rook: "\u265C",
-             black_bishop: "\u265D",
-             black_knight: "\u265E",
-             black_pawn: "\u265F",
-             _piece: "\u2610" }.freeze
-
   WHITE_TILES = %w[a2 a4 a6 a8 b1 b3 b5 b7 c2 c4 c6 c8 d1 d3 d5 d7 e2 e4 e6 e8 f1 f3 f5 f7 g2 g4 g6 g8 h1 h3 h5 h7].freeze
   BLACK_TILES = %w[a1 a3 a5 a7 b2 b4 b6 b8 c1 c3 c5 c7 d2 d4 d6 d8 e1 e3 e5 e7 f2 f4 f6 f8 g1 g3 g5 g7 h2 h4 h6 h8].freeze
 
@@ -134,7 +120,7 @@ module Display
     puts "\sa\sb\sc\sd\se\sf\sg\sh"
   end
 
-  def self.highlight_moves(moves, board)
+  def self.highlight_moves(board, moves)
     show(board, moves)
   end
 
